d9863972b5c2edebfc347b67f65ad878623b7c38
giacomosalvaderi <giacomo.salvaderi@mail.polimi.it>
2022-12-21 12:10:25 +0100

    JSON now working. event listener will now call function and parse id

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const open_1 = document.getElementById("open_1");
 
-function command() {
+function command(id) {
     let xhttp = new XMLHttpRequest();
+    let val = parseInt(id.split("_").pop().toString());
     let prova_json = {
-        relay : 1
+        relay : val
     };
+
     xhttp.open("POST", "/comando", true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(prova_json);
-    console.log("sent" + JSON.stringify(prova_json), prova_json)
+    xhttp.send(JSON.stringify(prova_json));
+    console.log("sent" + JSON.stringify(prova_json))
 }
-open_1.addEventListener("click", command);
+open_1.addEventListener("click", function () {
+    command(this.id)
+});
+
 console.log("ready")
+
+/*      GET request via AJAX -------------------------------------
+
+        document.addEventListener('DOMContentLoaded', function() {
+        // your code here
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+        document.getElementById("main_script").innerHTML = this.responseText;
+    }
+    };
+        xhttp.open("GET", "/index.js", true);
+        xhttp.send();
+    }, false);
+ */
--- webserver.js
+++ webserver.js
@@ -4,29 +4,32 @@ const io = require("socket.io-client");
 const app = express();
 const port = 8081;
 const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({strict: false}))
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
+app.get('/favicon.ico', function(req, res) {
+    res.sendFile(path.join(__dirname, '/assets/favicon.ico'));
+});
 app.get('/index.js', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.js'));
 });
-let jsonParser = bodyParser.json()
-app.use(bodyParser.urlencoded({ extended: false }));
-app.post('/comando', jsonParser, (req, res) => {
+
+app.post('/comando', (req, res) => {
     const jsonReq = req.body;
-    //const relay = JSON.parse(jsonReq);
-    console.log("recived: " + jsonReq.relay)
-    console.log("recived: " , jsonReq.relay)
-    const relay = jsonReq.relay;
-    const socket = io.connect('http://localhost:8080/arduino');
-    if(relay > 0 && relay < 9){
-        let val = parseInt(relay) + 4;
-        socket.emit(`relay:${val}`);
-        console.log(`Sent: ${val}`);
-        console.log(req.body)
+    if (jsonReq.hasOwnProperty("relay") && typeof(jsonReq.relay) == "number"){
+        console.log("received: " , jsonReq)
+        const relay = jsonReq.relay;
+        const socket = io.connect('http://localhost:8080/arduino');
+        if(relay > 0 && relay < 9){
+            let val = relay + 4;
+            socket.emit(`relay:${val}`);
+            console.log(`Sent: ${val}`, " = " + relay + " + offset(4)");
+        }
+        res.sendFile(path.join(__dirname, '/index.html'));
     }
-    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port);
